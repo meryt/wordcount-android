fedcb64139a6e6645fcd2de09ae046a16b5afdbc
meryt <meryt@meryt.com>
2014-01-10 14:18:15 -0800

    remove displaymessage stuff

--- src/com/meryt/android/wordcount/MainActivity.java
+++ src/com/meryt/android/wordcount/MainActivity.java
@@ -1,16 +1,12 @@
 package com.meryt.android.wordcount;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
+import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
-
-    public final static String EXTRA_MESSAGE = "com.meryt.android.wordcount.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +24,18 @@ public class MainActivity extends FragmentActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    /**
-     * Called when the user clicks the Send button
-     */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_calendar:
+                return true;
+            case R.id.action_projects:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupActionBar() {
